35a9ee160549aec919c33c9cf7a27f53669993f3
Rutujakarale <68233819+Rutujakarale@users.noreply.github.com>
2020-07-23 11:11:03 +0530

    Assignment 4 -day 4

---
+++ Day 4/script/prog2.js
@@ -0,0 +1,12 @@
+let {name,age,project:{diceGame}}=student;
+const student={
+    name="Helsinki",
+    age=24,
+    projects:{
+        diceGame:"Two player dice game using Javascript"
+    }
+}
+
+console.log(name);
+console.log(age);
+console.log(diceGame);
---
+++ Day 4/script/prog3.js
@@ -0,0 +1,7 @@
+let shopping_list=['Rice','Jawar','Bajra','cholates'];
+let shopping =[...shopping_list];
+shopping_list.push("ghee");
+shopping_list.push("oil");
+shopping_list.push("sugar");
+console.log("Shoping list:",shopping_list);
+console.log("shoping basket:",shopping);
